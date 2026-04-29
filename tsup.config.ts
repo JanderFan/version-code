173d35d2e19a0b9b2aff889d6e4fd6e75c553948
JanderFan <2928528663@qq.com>
2026-04-29 09:46:56 +0800

    1

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => {
+  return {
+    entry: ["src/index.ts"],
+    splitting: false,
+    sourcemap: !options.watch,
+    clean: true,
+    dts: true,
+    minify: !options.watch,
+  };
+});
